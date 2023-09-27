27b8bf52dec3bfc8afe947e3932a48ecbab21765
jonhelgi00 <jonhelgi12@gmail.com>
2023-09-27 16:22:26 +0200

    Cluster function initial idea

--- Vocab_tree.py
+++ Vocab_tree.py
@@ -5,7 +5,7 @@ class Tree:
 
   def __init__(self, data, depth, center) -> None:
     self.depth = depth #what is the current depth of the node
-    self.children = None
+    self.children = []
     self.center = center
     self.data = data
     self.logK_Ki = None
@@ -18,7 +18,12 @@ class Tree:
     #use KMeans algorithnm on data
     kmeans = KMeans(n_clusters=branch_factor, random_state=0, n_init="auto")
     labels = kmeans.fit_predict(self.data, y=None, sample_weight=None)
-    
+    for i in range(branch_factor):
+      # idx_i = [j for j,label in enumerate(labels) if label == i]
+      data_i = [d for j,d in enumerate(self.data) if labels[j] == i]
+      cluster_center_i = kmeans.cluster_centers_[i,:]
+      child = Tree(data=data_i, depth=self.depth + 1,center=cluster_center_i)
+      self.children.append(child)
     
 
   def IsLeafNode(self, max_depth):

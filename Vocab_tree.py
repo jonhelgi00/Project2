8b564b1c63f2fdfddd89f69e16aed2464506a880
jonhelgi00 <jonhelgi12@gmail.com>
2023-09-28 15:12:31 +0200

    Framework and data read

--- Vocab_tree.py
+++ Vocab_tree.py
@@ -1,9 +1,10 @@
 from sklearn.cluster import KMeans
+import pickle
 import numpy as np
 
 class Tree:
 
-  def __init__(self, data, depth, center) -> None:
+  def __init__(self, data, depth, center=None) -> None:
     self.depth = depth #what is the current depth of the node
     self.children = []
     self.center = center
@@ -12,7 +13,7 @@ class Tree:
 
 
   def IsEmpty(self):
-    return self.children == None
+    return self.data == None
   
   def cluster(self, branch_factor):
     #use KMeans algorithnm on data
@@ -29,10 +30,52 @@ class Tree:
   def IsLeafNode(self, max_depth):
     return max_depth == self.depth
   
-  def SetLogK_Ki(self):
-    pass
+  # def SetLogK_Ki(self):
+  #   pass
 
-  def Push(self):
-    pass
+  # def Push(self):
+  #   pass
 
+
+class HKMeans():
+  
+  def __init__(self,data, b, depth):
+    self.data = data
+    self.b = b
+    self.max_depth = depth
+    self.root = Tree(self.data, 0)
+    
+  
+  def ConstructVocab(self, data, b, depth, node):
+    if depth != 0 and not node.IsEmpty():
+      node.cluster(self.b)
+      for child in node.children:
+        self.ConstructVocab(child.data, self.b, depth-1, child)
+    else:
+      pass
+
+def Transform_data(filename):
+  # Read dictionary pkl file
+  # with open('database_ft.pkl', 'rb') as fp:
+  with open(filename, 'rb') as fp:
+    database_ft = pickle.load(fp)
+  
+  database_ft_ls = []
+  for key in database_ft:
+    for i in range(3):
+      for v in database_ft[key][i]:
+        database_ft_ls.append(v)
+      # print(len(database_ft[key][i]))
+  
+  return database_ft_ls
+
+
+def main():
+
+  database_data = Transform_data('database_ft.pkl')
+  
+  print(len(database_data))
+
+
+main()  
     
